d720a370c7325e441663b76b3d7f2edee2941fec
santix-l <137665289+santix-l@users.noreply.github.com>
2023-11-07 03:26:43 -0500

    Calculadora

--- src/main/java/gui/Calculator.java
+++ src/main/java/gui/Calculator.java
@@ -3,7 +3,6 @@ package gui;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -12,127 +11,163 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-public class Calculator extends VBox implements EventHandler<***>{
-	
-	String number1 = "";
-	String number2 = "";
-	String operator;
-	Text displayText;
-	
-	public Calculator(){
-		super(10);
-		this.displayText = new Text();
-		
-		Rectangle rt = new Rectangle(250, 50, Color.TRANSPARENT);
-		
-		rt.setStroke(Color.GRAY);
-		
-		StackPane sp =  new StackPane(rt, this.displayText);
-		
-		sp.setPadding(new Insets(10, 10, 10, 10));
-		
-		*** gd = new GridPane();
-		
-		gd.setPadding(new Insets(10, 10, 10, 10));
-		gd.setVgap(5);
-		gd.setHgap(4);
-		
-		gd.setAlignment(Pos.***);
-		
-		Button b7 = new ***("7");
-		gd.add(b7, 0, ***);
-		b7.setPrefWidth(50);
-		b7.setOnAction(this);
-		
-		*** b8 = new Button("8");
-		gd.add(b8, ***, 0);
-		b8.setPrefWidth(50);
-		b8.setOnAction(this);
-		
-		Button b9 = new Button(***);
-		gd.add(b9, 2, ***);
-		b9.setPrefWidth(50);
-		b9.setOnAction(this);
-		
-		Button div = new Button("/");
-		gd.add(div, ***, ***);
-		div.setPrefWidth(50);
-		div.setOnAction(***);
-		
-		*** b4 = new Button("4");
-		gd.add(b4, 0, 1);
-		b4.setPrefWidth(50);
-		b4.setOnAction(this);
-		
-		Button b5 = new ***("5");
-		gd.***(b5, 1, 1);
-		b5.setPrefWidth(50);
-		b5.setOnAction(this);
-		
-		Button b6 = new Button("6");
-		gd.add(b6, ***, ***);
-		b6.setPrefWidth(50);
-		***.setOnAction(this);
-		
-		*** mul = new Button("*");
-		gd.add(mul, ***, 1);
-		mul.setPrefWidth(50);
-		mul.setOnAction(this);
-		
-		Button b1 = new Button(***);
-		gd.add(b1, 0, ***);
-		b1.setPrefWidth(50);
-		b1.setOnAction(this);
-		
-		Button b2 = new ***("2");
-		gd.add(b2, ***, 2);
-		b2.setPrefWidth(50);
-		b2.setOnAction(this);
-		
-		Button b3 = new ***("3");
-		***.add(b3, 2, 2);
-		b3.setPrefWidth(50);
-		b3.setOnAction(***);
-		
-		*** minus = new Button("-");
-		gd.add(minus, 3, ***);
-		minus.setPrefWidth(50);
-		minus.setOnAction(this);
-		
-		Button b0 = new ***("0");
-		gd.add(b0, 0, 3, 2, 1);
-		b0.setPrefWidth(105);
-		***.setOnAction(this);
-		
-		Button plus = new Button("+");
-		gd.***(plus, ***, 3);
-		plus.setPrefWidth(50);
-		plus.setOnAction(this);
-		
-		Button equals = new Button(***);
-		***.add(equals, 3, 3);
-		equals.setPrefWidth(50);
-		equals.setOnAction(this);
-		
-		Button reset = new ***("C");
-		gd.add(reset, 0, 4, ***, 1);
-		reset.setPrefWidth(215);
-		reset.setOnAction(***);
-		
-		this.getChildren().addAll(sp, gd);
-	}
+public class Calculator extends VBox implements EventHandler<ActionEvent> {
 
-	@Override
-	public void ***(ActionEvent event) {
-		
-		Button b = (Button) event.getSource();
-		String value = b.getText();
-		
-		***
-		***
-		***
-		
-	}
-	
+    String number1 = "";
+    String number2 = "";
+    String operator;
+    Text displayText;
+
+    public Calculator() {
+        super(10);
+        this.displayText = new Text();
+
+        Rectangle rt = new Rectangle(250, 50, Color.TRANSPARENT);
+
+        rt.setStroke(Color.GRAY);
+
+        StackPane sp = new StackPane(rt, this.displayText);
+
+        sp.setPadding(new Insets(10, 10, 10, 10));
+
+        GridPane gd = new GridPane();
+
+        gd.setPadding(new Insets(10, 30, 10, 30));
+        gd.setVgap(5);
+        gd.setHgap(5); // Establece un margen horizontal entre las columnas.
+
+
+        Button b7 = new Button("7");
+        gd.add(b7, 0, 0);
+        b7.setPrefWidth(50);
+        b7.setOnAction(this);
+
+        Button b8 = new Button("8");
+        gd.add(b8, 1, 0);
+        b8.setPrefWidth(50);
+        b8.setOnAction(this);
+
+        Button b9 = new Button("9");
+        gd.add(b9, 2, 0);
+        b9.setPrefWidth(50);
+        b9.setOnAction(this);
+
+        Button div = new Button("/");
+        gd.add(div, 3, 0);
+        div.setPrefWidth(50);
+        div.setOnAction(this);
+
+        Button b4 = new Button("4");
+        gd.add(b4, 0, 1);
+        b4.setPrefWidth(50);
+        b4.setOnAction(this);
+
+        Button b5 = new Button("5");
+        gd.add(b5, 1, 1);
+        b5.setPrefWidth(50);
+        b5.setOnAction(this);
+
+        Button b6 = new Button("6");
+        gd.add(b6, 2, 1);
+        b6.setPrefWidth(50);
+        b6.setOnAction(this);
+
+        Button mul = new Button("*");
+        gd.add(mul, 3, 1);
+        mul.setPrefWidth(50);
+        mul.setOnAction(this);
+
+        Button b1 = new Button("1");
+        gd.add(b1, 0, 2);
+        b1.setPrefWidth(50);
+        b1.setOnAction(this);
+
+        Button b2 = new Button("2");
+        gd.add(b2, 1, 2);
+        b2.setPrefWidth(50);
+        b2.setOnAction(this);
+
+        Button b3 = new Button("3");
+        gd.add(b3, 2, 2);
+        b3.setPrefWidth(50);
+        b3.setOnAction(this);
+
+        Button minus = new Button("-");
+        gd.add(minus, 3, 2);
+        minus.setPrefWidth(50);
+        minus.setOnAction(this);
+
+        Button b0 = new Button("0");
+        gd.add(b0, 0, 3, 2, 1);
+        b0.setPrefWidth(600);
+        b0.setOnAction(this);
+
+        Button plus = new Button("+");
+        gd.add(plus, 2, 3);
+        plus.setPrefWidth(50);
+        plus.setOnAction(this);
+
+        Button equals = new Button("=");
+        gd.add(equals, 3, 3);
+        equals.setPrefWidth(50);
+        equals.setOnAction(this);
+
+        Button reset = new Button("C");
+        gd.add(reset, 0, 4, 4, 1);
+        reset.setPrefWidth(250);
+        reset.setOnAction(this);
+
+        this.getChildren().addAll(sp, gd);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        Button b = (Button) event.getSource();
+        String value = b.getText();
+
+        if (value.matches("[0-9]")) {
+            // Si el valor es un número, actualiza el número en pantalla.
+            displayText.setText(displayText.getText() + value);
+        } else if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
+            // Si el valor es un operador, guarda el primer número y el operador.
+            number1 = String.valueOf(Double.parseDouble(displayText.getText()));
+            operator = value;
+            displayText.setText(""); // Limpia la pantalla.
+        } else if (value.equals("=")) {
+            // Si se presiona "=" realiza la operación y muestra el resultado.
+            number2 = String.valueOf(Double.parseDouble(displayText.getText()));
+            double result = 0;
+
+            switch (operator) {
+                case "+":
+                    result = Double.parseDouble(number1) + Double.parseDouble(number2);
+                    break;
+                case "-":
+                    result = Double.parseDouble(number1) - Double.parseDouble(number2);
+                    break;
+                case "*":
+                    result = Double.parseDouble(number1) * Double.parseDouble(number2);
+                    break;
+                case "/":
+                    if (number2 != String.valueOf(0)) {
+                        result = Double.parseDouble(number1) / Double.parseDouble(number2);
+                    } else {
+                        displayText.setText("Error"); // Manejo de la división por cero.
+                        return;
+                    }
+                    break;
+            }
+           
+
+            displayText.setText(String.valueOf(result));
+        } else if (value.equals("C")) {
+            // Si se presiona "C", reinicia la calculadora.
+            number1 = String.valueOf(0);
+            number2 = String.valueOf(0);
+            operator = "";
+            displayText.setText("");
+        }
+    }
 
 }
+
